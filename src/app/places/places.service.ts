c484ab1008505a3d1a10a3839f4eb7d08bfc9af4
Claudio Godoy <clouds83@gmail.com>
2023-01-26 19:49:36 -0300

    update

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { Place } from "./place.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      "p1",
+      "Favelinha Doida",
+      "Humilde mas da hora",
+      "https://imagens.brasil.elpais.com/resizer/aHNMwo6qZ3IWdci83ZIy5kVxR2E=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/IQECV2K6IGKSCJOIZ6WVB45G3E.jpg",
+      9.9
+    ),
+    new Place(
+      "p2",
+      "Pico dos Boy",
+      "Para os ricos",
+      "https://uploads.metropoles.com/wp-content/uploads/2022/03/10113427/1-Casa-mais-cara-do-mundo_1.jpg",
+      899.9
+    ),
+    new Place(
+      "p3",
+      "Pindão Doidão",
+      "Só pra quem pode",
+      "https://marianaviaja.com/wp-content/uploads/2021/12/o-que-fazer-em-pindamonhangaba-sp.jpeg",
+      1899.9
+    ),
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() {}
+}
