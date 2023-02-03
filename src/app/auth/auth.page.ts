cf57544c2fa7c31a30dd11aacecede41e83a9c3d
Claudio Godoy <clouds83@gmail.com>
2023-02-02 22:00:31 -0300

    update

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { LoadingController } from "@ionic/angular";
 import { AuthService } from "./auth.service";
@@ -10,6 +11,7 @@ import { AuthService } from "./auth.service";
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -35,5 +37,26 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl("/places/tabs/discover");
         }, 1500);
       });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // send request to login
+    } else {
+      //send request to signup
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
