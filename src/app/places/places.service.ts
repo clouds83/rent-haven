12897cf11de91b607b88eca6a092e3a152d0048d
Claudio Godoy <clouds83@gmail.com>
2023-02-04 11:05:42 -0300

    update - datetime error

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       "Favelinha Doida",
       "Humilde mas da hora",
       "https://imagens.brasil.elpais.com/resizer/aHNMwo6qZ3IWdci83ZIy5kVxR2E=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/IQECV2K6IGKSCJOIZ6WVB45G3E.jpg",
-      9.9
+      9.9,
+      new Date("2023-01-01"),
+      new Date("2023-31-12")
     ),
     new Place(
       "p2",
       "Pico dos Boy",
       "Para os ricos",
       "https://uploads.metropoles.com/wp-content/uploads/2022/03/10113427/1-Casa-mais-cara-do-mundo_1.jpg",
-      899.9
+      899.9,
+      new Date("2023-01-01"),
+      new Date("2023-31-12")
     ),
     new Place(
       "p3",
       "Pindão Doidão",
       "Só pra quem pode",
       "https://marianaviaja.com/wp-content/uploads/2021/12/o-que-fazer-em-pindamonhangaba-sp.jpeg",
-      1899.9
+      1899.9,
+      new Date("2023-01-01"),
+      new Date("2023-31-12")
     ),
   ];
 
